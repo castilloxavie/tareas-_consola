87bfa4e9b3b528c97e527173c3da06e9555afcf8
castilloxavie <xcastillovaron93@gmail.com>
2025-07-15 17:06:33 -0500

    ajustes

--- helpers/guardarArchivos.js
+++ helpers/guardarArchivos.js
@@ -1,9 +1,14 @@
-const { log } = require("console");
 const fs = require("fs");
+const path = require("path");
 
-const directorio = "./db/datos.json"
+const directorio = "./db/datos.json";
 
 const guardarDatabase = (data) => {
+    // Crear la carpeta si no existe
+    const dir = path.dirname(directorio);
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
     fs.writeFileSync(directorio, JSON.stringify(data));
 }
 
@@ -15,8 +20,7 @@ const leerDatabase = () => {
     const info = fs.readFileSync(directorio, {encoding: "utf-8"});
     const data = JSON.parse(info);
 
-    return data
-    
+    return data;
 }
 
 module.exports = {
